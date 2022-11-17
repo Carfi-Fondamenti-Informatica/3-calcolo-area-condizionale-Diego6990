f77e16f8af5b0bb6a1583818978ac49855ba0673
Diego6990 <117745592+Diego6990@users.noreply.github.com>
2022-11-17 08:29:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
-
 int main() {
+   float a,b,x,y,z;
+   cin>>a>>b>>x>>y;
+   if(x<0 && y>0)
+   {
+   z=a*x-b*y;
+   }
+   else if(x>=0 && y<=0)
+   {
+   z=a*x*x-b*y;
+   }
+   else
+   {
+   z=a*x+b*y*y;
+   }
+   cout<<z<<endl;
    return 0;
 }
